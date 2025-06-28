4d78ec528d08e5713c47101f3ddb69f52002730a
Matthew Ricci <matthewricci552@gmail.com>
2025-06-28 16:40:33 -0700

    About to add infrastructure for Trial lists.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import { invoke } from "@tauri-apps/api/core";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './custom.scss';
-import { useState } from 'react';
-import { Box } from '@mui/system';
-import './App.css';
-
-export default App; //should this be here? In terms of, top of file or bottom or what
+import Button from '@mui/material/Button';
+import Box from '@mui/system/Box';
+import Container from '@mui/system/Container';
+import { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import Stack from '@mui/material/Stack';
 
 function App() {
   return (
@@ -18,68 +14,39 @@ function App() {
   );
 }
 
+
 interface TopBarProps {
 
 }
 function TopBar({}: TopBarProps) {
 
   return(
-    <Box
-    sx={{
-      display: 'grid',
-      gridTemplateColumns: 'auto auto auto'
-    }}
+    <Stack
+    direction="row"
     >
-      <span>
-        <button id={'addbutton'}>
-          <img 
-            src='src/assets/add_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg' 
-            style={{display:'inline', 
-            marginLeft:'auto', 
-            marginRight:'auto', 
-            width:'80%', 
-            height:'80%'}}>
-          </img>
-        </button>
-      </span>
+      <Container>
+        <Button>
+          <AddIcon
+            sx={{
+            }}>
+          </AddIcon>
+        </Button>
+      </Container>
 
-      <span style={{textAlign: 'center'}}>
+      <Container>
         <h6>+ Add Trial</h6>
-      </span>
+      </Container>
 
-      <span style={{textAlign: 'right'}}>
+      <Container>
         <h6>Settings</h6>
-      </span>
+      </Container>
 
-    </Box>
-
+    </Stack>
   );
 }
 
-interface ArenaScreenProps {
-  isDisplayed: Boolean,
-  index: number
-}
-function ArenaScreen({isDisplayed, index} : ArenaScreenProps) {
-  return (
-    <Box 
-    sx={{
-         display: isDisplayed ? 'display' : 'none',
-         height: '90vh', // So that the scrollbar doesn't get in the way of the tabs.
-         fontFamily: '"Lato", sans-serif',
-    }}>
-    <div className="arenascreen">
-      <h1>{`This is tab ${index}`}</h1>
-      <p>Yes, it is!</p>
-    </div>
-    </Box>
 
-  );
-}
-
-interface ArenaTabsProps {
-}
-function ArenaTabs({}: ArenaTabsProps) {
+function ArenaTabs() {
 
   function handleClick(curIndex: number) {
     setArenaSelected(curIndex);
@@ -92,68 +59,71 @@ function ArenaTabs({}: ArenaTabsProps) {
   const [whichArenaSelected, setArenaSelected] = useState<number>(0);
   const [numTabs, setNumTabs] = useState<number>(0);
 
-  let isDisplayed = true;
-  let arenaScreensArray: React.ReactElement[] = [];
-  for (let i = 0; i < numTabs+1; ++i) {
-    isDisplayed = i === whichArenaSelected ? true : false;
-    arenaScreensArray = arenaScreensArray.concat(<ArenaScreen isDisplayed={isDisplayed} index={i}></ArenaScreen>);
-  }
+  let indices: number[] = [];
 
-  let tabsArray: React.ReactElement[] = [];
-  for (let i = 0; i < numTabs-1; ++i) {
-    tabsArray = tabsArray.concat(<ArenaTab clickHandler={handleClick} index={i+1}></ArenaTab>);
+  for (let i = 0; i < numTabs+1; ++i) {
+    indices = indices.concat(i);
   }
+  console.log(indices);
+
+  const arenaScreensArray = indices.map((index) => 
+    <ArenaScreen isDisplayed={index === whichArenaSelected} index={index}></ArenaScreen>
+  );
+
+  const tabsArray = indices.slice(1).map((index) =>
+    <ArenaTab clickHandler={handleClick} index={index}></ArenaTab>
+  );
 
   return (
-    <div style={{whiteSpace: 'nowrap'}}>
+    <Box 
+    sx={{whiteSpace: 'pre'}}>
       {arenaScreensArray}
 
-      <Box
-      component="button"
-      sx={{
-        bgcolor: '#555',
-        color: 'white',
-        border: 'none',
-        outline: 'none',
-        cursor: 'pointer',
-        fontSize: 17,
-        ":hover": {
-          bgcolor: '#777'
-        },
-      }}
+      <Button
+      sx={{}}
       onClick={addTab}
       >
-      <img src='src/assets/add_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg' style={{display:'inline', marginLeft:'auto', marginRight:'auto', width:'80%', height:'80%'}}></img>
-      </Box>
+      <AddIcon></AddIcon>
+      </Button>
 
       {tabsArray}
-    </div>
+    </Box>
   );
 }
 
 
+interface ArenaScreenProps {
+  isDisplayed: Boolean,
+  index: number
+}
+function ArenaScreen({isDisplayed, index} : ArenaScreenProps) {
+  return (
+    <Box 
+    sx={{
+      display: isDisplayed ? 'block' : 'none',
+      height: '80vh', // So that the scrollbar doesn't get in the way of the tabs.
+      fontFamily: '"Lato", sans-serif',
+    }}>
+      <h1>{`This is tab ${index}`}</h1>
+      <p>Yes, it is!</p>
+    </Box>
+
+  );
+}
+
 interface ArenaTabProps {
-  clickHandler: (arg0: number) => void,
+  clickHandler: (index: number) => void,
   index: number
 }
 function ArenaTab ({clickHandler, index}: ArenaTabProps) { // how 2 isDisplayed
   return (
-      <Box
-      component="button"
-      sx={{
-        bgcolor: 'white',
-        borderTop: 'none',
-        cursor: 'pointer',
-        fontSize: 17,
-        fontFamily: '"Lato", sans-serif',
-        ":hover": {
-          bgcolor: '#777'
-        },
-        borderRadius: '0px 0px 10px 10px'
-      }}
+      <Button
+      sx={{}}
       onClick={() => clickHandler(index)}
       >
         {`Tab ${index}`}
-      </Box>
+      </Button>
   );
 }
+
+export default App;
