6a65140284b23d55e063ef834e56d37abb7edfa5
Matthew Ricci <matthewricci552@gmail.com>
2025-07-14 16:01:18 -0700

    Add arena dialogue's basic design complete, is able to capture value.

--- src/Dialogs.tsx
+++ src/Dialogs.tsx
@@ -8,12 +8,22 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import RemoveIcon from '@mui/icons-material/Remove';
 import AddIcon from '@mui/icons-material/Add';
+import { useState } from "react";
 
 interface AddArenaDialogProps {
     open: boolean,
     onClose: () => void;
 }
 function AddArenaDialog({open, onClose}: AddArenaDialogProps) {
+  const [value, setValue] = useState("");
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  }
+
+  const addArenaFormSubmit = () => {
+    console.log(value);
+  }
 
   return (
     <Dialog 
@@ -38,7 +48,8 @@ function AddArenaDialog({open, onClose}: AddArenaDialogProps) {
             >
             <CloseIcon />
             </IconButton>
-                <TextField id="outlined-basic" label="Arena Title" variant="outlined" sx={{
+                <TextField id="outlined-basic" label="Arena Title" variant="outlined" value={value} onChange={handleInput} 
+                sx={{
                     paddingBottom: '10px'
                 }}/>
                 <Typography sx={{
@@ -56,7 +67,7 @@ function AddArenaDialog({open, onClose}: AddArenaDialogProps) {
                 }}>
                     Stuff
                 </Box>
-                <Button variant="contained">Submit</Button>
+                <Button variant="contained" onClick={addArenaFormSubmit}>Submit</Button>
         </Box>
     </Dialog>
   );
